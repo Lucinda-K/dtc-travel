b1d82853a5b2e3730c3fb9897c3a664d04ccb53a
Quyen Castellanos <Quyen.Castellanos@davita.com>
2017-07-30 19:11:28 -0500

    Set up for collection views within category cards

---
+++ dtc-travel-app/dtc-travel-app/CategoryTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryTableViewCell.swift
+//  dtc-travel-app
+//
+//  Created by Quyen Castellanos on 7/30/17.
+//  Copyright © 2017 Pariveda. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // self.collectionView.dataSource = self
+        // self.collectionView.delegate = self
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+}
---
+++ dtc-travel-app/dtc-travel-app/TripBoardViewController.swift
@@ -0,0 +1,71 @@
+//
+//  TripBoardViewController.swift
+//  dtc-travel-app
+//
+//  Created by Quyen Castellanos on 7/30/17.
+//  Copyright © 2017 Pariveda. All rights reserved.
+//
+
+import UIKit
+
+class TripBoardViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    final fileprivate let reuseId = "categoryCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.register(UINib(nibName: "CategoryCell", bundle: nil), forCellReuseIdentifier: reuseId)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TripBoardViewController: UITableViewDataSource, UITableViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "FOOD"
+        case 1:
+            return "PEOPLE"
+        case 2:
+            return "BUILDINGS"
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseId, for: indexPath) as? CategoryTableViewCell else {
+            preconditionFailure("Cell cannot be dequeued as a CategoryTableViewCell")
+        }
+        
+        // cell.textLabel?.text = "hello world!"
+        return cell
+    } 
+}
